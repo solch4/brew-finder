8a66c5289f15e9525c70e71a5c7efe39e3f2ceaf
“solch4” <sol.maldonado.0209@gmail.com>
2024-04-17 22:15:04 -0300

    Header btns updated

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,11 @@
 import { useRouter } from "next/router";
 import { BiMenu, BiSolidBell, BiSolidUser } from "react-icons/bi";
 import { MdOutlineArrowBack } from "react-icons/md";
-import { Flex, IconButton, Spacer } from "@chakra-ui/react";
+import { Flex, IconButton, IconButtonProps, Spacer } from "@chakra-ui/react";
+
+function HeaderButton(props: IconButtonProps) {
+  return <IconButton variant="ghost" fontSize={24} {...props} />;
+}
 
 function Header() {
   const router = useRouter();
@@ -10,22 +14,17 @@ function Header() {
   return (
     <Flex as={"header"} position={"sticky"} top={0} bg={"dark.900"} p={2}>
       {shouldRenderBackBtn ? (
-        <IconButton
+        <HeaderButton
           aria-label="Volver"
-          fontSize={24}
           icon={<MdOutlineArrowBack />}
           onClick={router.back}
         />
       ) : (
-        <IconButton aria-label="Menu" fontSize={24} icon={<BiMenu />} />
+        <HeaderButton aria-label="Menu" icon={<BiMenu />} />
       )}
       <Spacer />
-      <IconButton
-        aria-label="Notificaciones"
-        fontSize={24}
-        icon={<BiSolidBell />}
-      />
-      <IconButton aria-label="Perfil" fontSize={24} icon={<BiSolidUser />} />
+      <HeaderButton aria-label="Notificaciones" icon={<BiSolidBell />} />
+      <HeaderButton aria-label="Perfil" icon={<BiSolidUser />} />
     </Flex>
   );
 }
