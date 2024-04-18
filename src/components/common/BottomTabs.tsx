0f4c348f97f142c64117370c689a1c74f3934679
“solch4” <sol.maldonado.0209@gmail.com>
2024-04-18 03:42:40 -0300

    BottomTabs style updated

--- src/components/common/BottomTabs.tsx
+++ src/components/common/BottomTabs.tsx
@@ -3,14 +3,22 @@ import { BsChat } from "react-icons/bs";
 import { RxCalendar } from "react-icons/rx";
 import { MdHomeFilled } from "react-icons/md";
 import { As, Flex, Icon as ChakraIcon, Link } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+const tabs = [
+  { label: "Calendario", href: "/calendar", Icon: RxCalendar },
+  { label: "Inicio", href: "/", Icon: MdHomeFilled },
+  { label: "Chat", href: "/chat", Icon: BsChat },
+];
 
 interface TabButtonProps {
   Icon: As;
   label: string;
   href: string;
+  isActive: boolean;
 }
 
-function TabButton({ Icon, label, href }: TabButtonProps) {
+function TabButton({ Icon, label, href, isActive }: TabButtonProps) {
   return (
     <Link
       as={NextLink}
@@ -20,6 +28,7 @@ function TabButton({ Icon, label, href }: TabButtonProps) {
       flexDirection="column"
       alignItems="center"
       fontSize="xs"
+      color={isActive ? "primary" : "white"}
     >
       <ChakraIcon as={Icon} fontSize={24} aria-hidden />
       {label}
@@ -28,11 +37,13 @@ function TabButton({ Icon, label, href }: TabButtonProps) {
 }
 
 function BottomTabs() {
+  const router = useRouter();
+
   return (
     <Flex position={"sticky"} bottom={0} bg={"dark.900"} p={2}>
-      <TabButton label="Calendario" href="/calendar" Icon={RxCalendar} />
-      <TabButton label="Inicio" href="/" Icon={MdHomeFilled} />
-      <TabButton label="Chat" href="/chat" Icon={BsChat} />
+      {tabs.map((tab, i) => (
+        <TabButton isActive={router.pathname === tab.href} {...tab} key={i} />
+      ))}
     </Flex>
   );
 }
